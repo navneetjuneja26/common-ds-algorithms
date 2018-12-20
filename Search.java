aa2b0e46a90f68f81948484b88e5e82b9a0096cd
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-12-20 16:40:12 +0530

    added another way for whether elem exists or not

--- Search.java
+++ Search.java
@@ -5,18 +5,32 @@ public class Search {
         int []intArray = {20,35,-15,7,55,1,-22};
         int index = linearSearch(intArray,35);
         System.out.println("Element found at the index " + index);
+        boolean found = justFind(intArray,3121);
+        System.out.println(found);
     }
+//    Worst case is for the very last item in the array ( o(n) )
+    // Best case is when the element is found at the very first index of the array
     public static int linearSearch(int arr[],int elem) {
-        boolean found = false;
+
         int index = -1;
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] == elem) {
-                found = true;
                 index = i;
                 break;
             }
         }
         return index;
+    }
+    public static boolean justFind(int arr[],int elem) {
+        final boolean[] found = {false};
+        Arrays.stream(arr).forEach(e -> {
+            if(found[0] != true) {
+                if (e == elem) {
+                    found[0] = true;
+                }
+            }
+        });
+        return found[0];
     }
 
 }
